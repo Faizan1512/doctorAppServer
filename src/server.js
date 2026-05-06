8d1c547b9b4d921704ef023f527b9de9c7e6ccf4
faizan <faizanmubarak13@gmail.com>
2026-05-06 17:25:17 +0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -47,7 +47,7 @@ app.get("/api/health", (req, res) => res.status(200).json({ message: "Success" }
 // Serve frontend in production
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../admin/dist")));
-  app.get("/*", (req, res) => {
+  app.get("/{*path}", (req, res) => {
     res.sendFile(path.join(__dirname, "../admin/dist/index.html"));
   });
 }
